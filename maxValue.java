f834b525d6247c677bb590d1d1179cc7a26a32d3
Erik <3rikthered.main@gmail.com>
2019-01-26 10:23:06 -0500

    Update maxValue.java

--- maxValue.java
+++ maxValue.java
@@ -1,39 +1,67 @@
 import java.util.Scanner;
 
-class Main
+public class nelsonTask1
 {
-  public static void main(String[] args)
-  {
-    Scanner input = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		// initialize scanner
+		Scanner input = new Scanner(System.in);
+		
+		// initialize rows and columns
+		int arrayRows;
+		int arrayColumns;
+		
+		// prompt for total rows
+		System.out.print("How many rows are in this array? ");
+		arrayRows = input.nextInt();
+		
+		// prompt for total columns
+		System.out.print("How many columns are in this array? ");
+		arrayColumns = input.nextInt();
+		
+		// generate array
+		double[][] a = new double[arrayRows][arrayColumns];
+		
+		// prompt for array values and place them in the array
+		for (int rows = 0; rows < a.length; rows++)
+		{
+			System.out.println("Please enter " + arrayColumns +
+							   " values for row " + (rows + 1) +
+							   ": ");
+			for (int columns = 0; columns < a[0].length; columns++)
+			{
+				a[rows][columns] = input.nextDouble();
+			}
+		}
+		
+		// Use the locateLargest method on the array
+		Location locateLargestValue = locateLargest(a);
+		
+		
+		
+	}
 
-    Location locationFinder = new Location();
-
-    System.out.print("Array dimensions: How many rows? ");
-    int arrayRows = input.nextInt();
-
-    System.out.print("Array dimensions: How many columns? ");
-    int arrayColumns = input.nextInt();
-
-    // Generate the array
-    double[][] a = new double[arrayRows][arrayColumns];
-
-    // assign user input values for each spot in the array
-    for (int row = 0; row < a.length; row++)
-    {
-      System.out.print("Please enter " + arrayColumns + " values for row " + (row + 1) + ": ");
-      for (int column = 0; column < a[0].length; column++)
-      {
-        array[row][column] = input.nextDouble();
-      }
-    }
-
-    System.out.print(Location(a));
-  }
+	public static Location locateLargest(double[][] a)
+	{
+		double maxValue = a[0][0];
+		
+		for (int rows = 0; rows < a.length; rows++)
+		{
+			for (int columns = 0; columns < a[0].length; columns++)
+			{
+				if (a[rows][columns] > maxValue)
+				{
+					maxValue = a[rows][columns];
+				}
+			}
+		}
+		return maxValue;
+	}
 }
 
-class Location
+public class Location
 {
-  int row;
-  int column;
-  double maxValue;
+	public int row;
+	public int column;
+	public double maxValue;
 }
