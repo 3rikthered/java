24cd76ae2f79f3d1bd7cbb82027d912b772f457d
Erik <3rikthered.main@gmail.com>
2019-02-01 10:37:37 -0500

    Update splitty.java

--- splitty.java
+++ splitty.java
@@ -1,42 +1,3 @@
-/*
-
-The split method in the String class returns an array of strings consisting of the substrings split by the
-delimiters. However, the delimiters are not returned.
-Implement the following new method that returns an array of strings consisting of the substrings split
-by the matching delimiters, including the matching delimiters.
-Assume a maximum limit of 2 delimiters, the space denotes end of string and beginning of delimiters.
-- public static String[] split(String s, String regex)
-For example, split(”ab#12#453”, ”#”) returns
-- ab, #, 12, #, 453 in an array of String
-and split(”a?b?gfe#”, ”[?#]”) returns
-- a, ?, b, ?, gfe, #, in an array of String.
-
-*/
-
-// My solution:
-// Take the string in
-// for each character [regex]
-//    insert a Splitter before and after each [regex]
-// Split string by Splitter
-// Return results
-
-
-
-/*
-
-split string into array of strings, separated by nothing
-for each variable in the array:
-  if variable == [regex]:
-    variable = Splitter [regex] Splitter
-
-Turn array of strings back into string
-Split string by Splitter
-Return results
-
-LOOK INTO string.replaceAll()
-
-*/
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -67,7 +28,6 @@ class Main
   
   }
 
-  // Regex method, converts characters
 
   // Splitter method, accepts a string and delimiters
   public static String[] split(String s, String regex)
@@ -75,9 +35,21 @@ class Main
     // Separate the regex into an array
     String[] delimiters = regex.split("");
 
+    // Fix certain characters in the array that don't play nicely otherwise
+    for (int i = 0; i < delimiters.length; i++)
+    {
+      switch(delimiters[i])
+      {
+        case "$": delimiters[i] = "\\$"; break;
+        case "^": delimiters[i] = "\\^"; break;
+
+      }
+    }
+
     // Searches for all regex delimiters in the string,
     // Adds a new delimiter on each side of the regex delimiter
-    for (int i = 0; i < delimiters.length; i++){
+    for (int i = 0; i < delimiters.length; i++)
+    {
           s = s.replaceAll(delimiters[i], "!!SPLITHERE!!" + delimiters[i] + "!!SPLITHERE!!");
     }
 
