990a0f16866102c9720309841e82a9078d8ee047
Erik <3rikthered.main@gmail.com>
2019-04-13 21:57:36 -0400

    Update Postfix.java

--- Postfix.java
+++ Postfix.java
@@ -1,57 +1,62 @@
+package nelsonTask7;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.TreeSet;
 
-public class Postfix
+public class nelsonTask7
 {
-	
-	static Scanner input = new Scanner(System.in);
-
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws Exception
+	{	
+		Scanner input = new Scanner(System.in);
+		
 		int choice = 0;
 		while (choice != 4)
 		{
-			choice = setChoice();
+			choice = setChoice(input);
 			if (choice == 4)
 				break;
-			menu(choice);
+			menu(choice, input);
 		}
+		
+		input.close();
 	}
 
-	private static void menu(int choice)
-	{
+	private static void menu(int choice, Scanner input) throws Exception
+	{		
 		if (choice == 1)
 		{
-			System.out.println("--Evaluate postfix expression--\n\n" +
+			System.out.print("--Evaluate postfix expression--\n\n" +
 							 "Enter a postfix expression: ");
 			
-			String postfixExpression = input.next();
-			
-			System.out.print("\n");
+			String postfixExpression = reader(input);
 			postfix(postfixExpression);
 		}
 		else if (choice == 2)
 		{
 			System.out.print("Convert infix to postfix and evaluate\n\n" +
 							 "Enter an infix expression: ");
-			
-			String infixExpression = input.nextLine();
-			
-			System.out.print("\n");
+
+			String infixExpression = reader(input);
 			infix2postfix(infixExpression);
 		}
 		else if (choice == 3)
 		{
-			System.out.print("Alphebetize and dedupe words from text file\n\n" +
-							 "Enter the filepath: ");
+			System.out.print("Alphebetize and dedupe words from text file\n\n");
 			
-			// IMPORT FILE
+			fileReader(input);
 		}
 	}
 
-	private static int setChoice()
-	{
+	private static int setChoice(Scanner input)
+	{		
 		int choice = 0;
 		
 		while (choice < 1 || choice > 4)
@@ -59,7 +64,7 @@ public class Postfix
 			System.out.println("Choose a method:\n\n" +
 							   "1 - Evaluate postfix expression\n" +
 							   "2 - Convert infix to postfix and evaluate\n" +
-							   "3 - Alphebetize and dedupe words from text file" +
+							   "3 - Alphebetize and dedupe words from text file\n" +
 							   "4 - Exit");
 			
 			choice = input.nextInt();
@@ -67,48 +72,66 @@ public class Postfix
 		
 		return choice;
 	}
+	
+	// clears 'Enter' from the buffer, then reads the input 
+	public static String reader(Scanner input)
+	{
+		input.nextLine();
+		return input.nextLine();
+	}
 
+	
 	/////////////////////////////////
 	// Evaluate postfix expression //
 	/////////////////////////////////
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! this code works, the input is failing somewhere
-	private static void postfix(String postfixExpression) {
-		
-		System.out.print(postfixExpression);
-		
+	private static void postfix(String postfixExpression)
+	{				
 		Stack<Integer> stack = new Stack<>();
-		
-		for (int i = 0; i < postfixExpression.length(); i++)
+		List<String> parsed = parser(postfixExpression);
+			
+		for (int i = 0; i < parsed.size(); i++)
 		{
-			char value = postfixExpression.charAt(i);
+			String value = parsed.get(i);
 			
-			
-			if (Character.isDigit(value))
-			{
-				stack.push(value - '0');
-			}
-			else
+			if (operatorsList.contains(value))
 			{
 				switch(value)
 				{
-					case '+': stack = addition(stack);
+					case "+": stack = addition(stack);
 							  break;
-					case '-': stack = subtraction(stack);
+					case "-": stack = subtraction(stack);
 							  break;
-					case '/': stack = division(stack);
+					case "*": stack = multiplication(stack);
 						  	  break;
-					case '*': stack = multiplication(stack);
+					case "/": stack = division(stack);
 						  	  break;
-					case ' ': break;
+					case "^": stack = exponent(stack);
 				}
+			}
+			
+			else
+			{
+				stack.push(Integer.valueOf(value));
 			}
 		}
 		
-		System.out.print("\n\n" + postfixExpression + " = " + stack.pop() + "\n\n");
+		System.out.print("\n" + postfixExpression + " = " + stack.pop() + "\n\n");
 	}
 
-	private static Stack<Integer> multiplication(Stack<Integer> stack) {
+///////////////////////////////////////////////////////////////////////////////////////////////
+	// MATHEMATIC OPERATORS: pop the last 2 elements in the stack, then perform the appropriate
+	// mathematic operation against them.
+	private static Stack<Integer> exponent(Stack<Integer> stack) {
+		int lastInt = stack.pop();
+		int secondLastInt = stack.pop();
 		
+		stack.push((int) Math.pow(secondLastInt, lastInt));
+		
+		return stack;
+	}
+
+	private static Stack<Integer> multiplication(Stack<Integer> stack)
+	{
 		int lastInt = stack.pop();
 		int secondLastInt = stack.pop();
 		
@@ -117,7 +140,8 @@ public class Postfix
 		return stack;
 	}
 
-	private static Stack<Integer> division(Stack<Integer> stack) {
+	private static Stack<Integer> division(Stack<Integer> stack)
+	{
 		int lastInt = stack.pop();
 		int secondLastInt = stack.pop();
 		
@@ -126,7 +150,8 @@ public class Postfix
 		return stack;
 	}
 
-	private static Stack<Integer> subtraction(Stack<Integer> stack) {
+	private static Stack<Integer> subtraction(Stack<Integer> stack)
+	{
 		int lastInt = stack.pop();
 		int secondLastInt = stack.pop();
 		
@@ -135,7 +160,8 @@ public class Postfix
 		return stack;
 	}
 
-	private static Stack<Integer> addition(Stack<Integer> stack) {
+	private static Stack<Integer> addition(Stack<Integer> stack)
+	{
 		int lastInt = stack.pop();
 		int secondLastInt = stack.pop();
 		
@@ -143,12 +169,191 @@ public class Postfix
 		
 		return stack;
 	}
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 	///////////////////////////////////////////
 	// Convert infix to postfix and evaluate //
 	///////////////////////////////////////////
-	private static void infix2postfix(String infixExpression) {
+	private static void infix2postfix(String infixExpression)
+	{		
+		Stack<String> stack = new Stack<>();
+		List<String> parsed = parser(infixExpression);
+
+		String postfixExpression = "";
 		
+		// checks each element in the parsed user input
+		for (int i = 0; i < parsed.size(); i++)
+		{
+			String value = parsed.get(i);
+						
+			switch(value)
+			{
+				// Opening parentheses are pushed onto the stack
+				case "(": stack.push(value);
+						  break;
+				// Ending parentheses ensure that parenthetical expressions are evaluated
+				// as a group, up until it reaches the opening parenthesis
+				case ")": while(!stack.isEmpty() && stack.peek() != "(")
+						  {
+								postfixExpression += stack.pop();
+						  }
+						  break;
+				// Ugly code having an if-else statement inside a case switch, I haven't
+				// figured out how to implement it differently though. If the element is
+				// an operator, it will determine precedence and order the operations
+				// accordingly. If it is a number, it will place the value in the postfix
+				// expression.
+				default:  if (operatorsList.contains(value))
+						  {
+						  		while (!stack.isEmpty() && precedence(value) <= precedence(stack.peek()))
+						  		{
+						  			postfixExpression += stack.pop();
+						  		}
+						  		stack.push(value);
+						  }
+						  else
+						  {
+							  	postfixExpression += (value + " ");
+						  }
+						  break;
+			}
+		}
+		
+		while (!stack.isEmpty())
+		{
+			postfixExpression += stack.pop();
+		}
+		
+		// Removes beginning parentheses from the postfixExpression
+		postfixExpression = postfixExpression.replaceAll("\\(", " ");
+		
+		System.out.print("\n" + infixExpression + " = " + postfixExpression);
+		postfix(postfixExpression);
 	}
 
+	// order of precedence for operators
+	private static int precedence(String value)
+	{
+		switch (value)
+		{
+			case "+": return 1;
+			case "-": return 1;
+			case "*": return 2;
+			case "/": return 2;
+			case "^": return 3;
+			default:  return 0;
+		}
+	}
+	
+	// parses the input into an arraylist
+	private static List<String> parser(String input)
+	{
+		// because people don't always put spaces between operators and numbers, this
+		// pads each operator with spaces for parsing
+		input = input.replace("+", " + ");
+		input = input.replace("-", " - ");
+		input = input.replace("*", " * ");
+		input = input.replace("/", " / ");
+		input = input.replace("^", " ^ ");
+		input = input.replace("(", " ( ");
+		input = input.replace(")", " ) ");
+
+		// splits by spaces
+		List<String> list = new ArrayList<String>(Arrays.asList(input.split(" ")));
+		
+		// removes empty elements created as a result of double spaces
+		list.removeAll(Arrays.asList("", null));
+		return list;
+	}
+	
+	// A list of mathematic operators to compare against
+	static List<String> operatorsList = new ArrayList<String>(Arrays.asList("+", "-", "*", "/", "^"));
+
+	
+	/////////////////////////////////////////////////
+	// Alphebetize and dedupe words from text file //
+	/////////////////////////////////////////////////
+	public static void fileReader(Scanner input) throws Exception
+	{
+		System.out.print("Enter the name of the text file: ");
+		String fileName = reader(input);
+		
+		LinkedList<String> list = importer(fileName, input);
+		
+		// alphebetizes the list
+		list.sort(String::compareToIgnoreCase);
+		
+		// dedupes the list
+		list = dedupe(list);
+		
+		// Prints the list out in descending alphabetic order
+		//!!!!!!!!!!!!!!!!!!! FIND A WAY TO PRINT FOUR WORDS PER LINE WITHOUT THROWING EXCEPTION
+		ListIterator<String> iterator = list.listIterator(list.size());
+		while (iterator.hasPrevious())
+		{
+			System.out.println(iterator.previous());
+		}
+	}
+
+	// Imports the file and reads it into the LinkedList
+	public static LinkedList<String> importer(String fileName, Scanner input) throws Exception
+	{
+		LinkedList<String> list = new LinkedList<String>();
+		
+		try
+		{
+			// check if the filename is correct
+			if (fileName.equals("hangman.txt"))
+			{
+				// reads the file
+				BufferedReader readFile = new BufferedReader(new FileReader(fileName));
+				
+				String nextWord = "";
+				
+				// while there are still words in the file, adds those words to the LinkedList
+				while ((nextWord = readFile.readLine()) != null)
+				{
+					list.add(nextWord);
+				}
+				
+				readFile.close();
+			}
+			else
+			{
+				throw(new FileNotFoundException());
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		return list;
+	}
+	
+	// EXCEPTION HANDLER
+	public static class FileNotFoundException extends Exception
+	{
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public String getMessage()
+		{
+			return "EXCEPTION: File must be \"hangman.txt\"\n";
+		}
+	}
+	
+	// To dedupe the linkedlist, converts it to a case-insensitive treeset, then converts back.
+	public static LinkedList<String> dedupe(LinkedList<String> list)
+	{
+		TreeSet<String> set = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+		
+		for (String string : list)
+		{
+			set.add(string);
+		}
+		
+		LinkedList<String> returnList = new LinkedList<String>(set);
+		return returnList;
+	}
 }
